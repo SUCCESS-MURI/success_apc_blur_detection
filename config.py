6b0a94e024d0a086072d219c94edd9d4156e0b6a
Mary Hatfalvi <mthatfalvi@wpi.edu>
2022-01-25 14:55:07 -0500

    Update config.py

--- config.py
+++ config.py
@@ -19,11 +19,11 @@ config.TRAIN.lr_decay = 0.8
 config.TRAIN.decay_every = 6000
 
 ## train set location
-config.TRAIN.CUHK_blur_path = '/home/mhatfalv/local_success_dataset/image/'
-config.TRAIN.CUHK_gt_path = '/home/mhatfalv/local_success_dataset/gt/'
+config.TRAIN.CUHK_blur_path = '/dataset/CHUK/image/'
+config.TRAIN.CUHK_gt_path = '/dataset/CHUK/gt/'
 
-config.TRAIN.synthetic_blur_path = '/home/mhatfalv/local_success_dataset/BlurTrainSyntheic/imgs/'
-config.TRAIN.synthetic_gt_path = '/home/mhatfalv/local_success_dataset/BlurTrainSyntheic/GT/'
+config.TRAIN.synthetic_blur_path = '/dataset/synthetic/imgs/'
+config.TRAIN.synthetic_gt_path = '/dataset/synthetic/GT/'
 
 config.TEST.blur_path = '/Dataset/BlurDetection/CUHK/test/imgs/'
 config.TEST.gt_path = '/Dataset/BlurDetection/CUHK/test/GT/'
